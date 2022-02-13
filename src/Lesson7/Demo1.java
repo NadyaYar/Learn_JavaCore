533ff8aa14ce793a8505dba1e3888c39aa102121
NadyaYar <nadya.yarmak03@gmail.com>
2022-02-13 16:25:58 +0200

    add work

---
+++ src/Lesson7/Demo1.java
@@ -0,0 +1,20 @@
+package Lesson7;
+
+import Lesson6.Deal;
+
+import java.util.Date;
+
+public class Demo1 {
+    public Deal createDeal() {
+
+        return new Deal(100, new Date(), false, null, "Dnepr", "Ukraine", "Buy");
+    }
+
+    public Deal createDealAndCallMethods() {
+        Deal deal = new Deal(100, new Date(), true, new Date(), "Kiev", "Ukraine", "SomeValue");
+        deal.setConfirmOder();
+        deal.checkPrice();
+        deal.isValidType();
+        return deal;
+    }
+}
