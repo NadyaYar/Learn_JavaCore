4560ffcd2d4061234a7b92bbb9ea684e6b8dc7dd
NadyaYar <nadya.yarmak03@gmail.com>
2022-02-16 10:40:50 +0200

    add work

--- src/Lesson8/students/Demo.java
+++ src/Lesson8/students/Demo.java
@@ -7,7 +7,9 @@ public class Demo {
     }
 
     public static SpecialStudents createLowestChild() {
-        return new SpecialStudents("Nadya", "Yarmak", 13, new String[]{"Math", "English", "Web developer"}, "Dut", 10, 12345, 123468, "nadya.yarmak03@gmail.com");
+        return new SpecialStudents("Nadya", "Yarmak", 13,
+                new String[]{"Math", "English", "Web developer"},
+                "Dut", 10, 12345, 123468, "nadya.yarmak03@gmail.com");
     }
 }
 
