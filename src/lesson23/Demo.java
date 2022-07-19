37f95109391e62193faf4fbdf3637e9a63f84ffe
NadyaYar <nadya.yarmak03@gmail.com>
2022-07-19 17:46:58 +0300

    final

---
+++ src/lesson23/Demo.java
@@ -0,0 +1,11 @@
+package lesson23;
+
+public class Demo {
+    public static void main(String[] args) {
+        Validator validator = new Validator("user");
+        System.out.println(validator.getCheckCount());
+
+        Validator validator1 = new Validator("user");
+        System.out.println(validator1.getCheckCount());
+    }
+}
---
+++ src/lesson23/StreamValidator.java
@@ -0,0 +1,14 @@
+package lesson23;
+
+public class StreamValidator{
+
+//    public StreamValidator(String name) {
+//        super(name);
+//    }
+
+//    @Override
+//    public boolean validate() {
+//        // other logic
+//        return false;
+//    }
+}
